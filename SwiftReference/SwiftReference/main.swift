7eaab30073727ac5ecb346f43068272b30ca2734
cos1000 <matt_choy@hotmail.com>
2015-01-14 21:25:28 +0800

    First

---
+++ SwiftReference/SwiftReference/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftReference
+//
+//  Created by Matt Choy on 14/1/2015.
+//  Copyright (c) 2015年 Matt Choy. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
